5f52172fbcb94de4455d8c1f8e18e75900fde649
Raquel Fernandes <raquelgfernandes88@gmail.com>
2023-11-05 15:24:33 -0100

    Doxygen .h

--- Classes/Schedule.h
+++ Classes/Schedule.h
@@ -1,30 +1,75 @@
-
 #ifndef AED_PROJECT_SCHEDULE_H
 #define AED_PROJECT_SCHEDULE_H
 #include "Lesson.h"
 #include <vector>
 
+/**
+ * @class Schedule
+ * @brief Classe que representa um horário de aulas.
+ */
 class Schedule {
 private:
-    std::vector<Lesson> horario;
+    std::vector<Lesson> horario; ///< Vetor de aulas no horário.
     std::string formathours(float hours);
+
 public:
+    /**
+     * @brief Construtor padrão que inicializa um horário vazio.
+     */
     Schedule();
+
+    /**
+     * @brief Destrutor da classe Schedule.
+     */
     ~Schedule();
+
+    /**
+     * @brief Construtor que inicializa um horário com um vetor de aulas.
+     * @param horario Vetor de aulas a ser associado ao horário.
+     */
     Schedule(std::vector<Lesson> horario);
 
-    //operators
-    bool operator == (const Schedule& s) const;
+    // Operadores
+
+    /**
+     * @brief Sobrecarga do operador de igualdade para comparar dois horários.
+     * @param s Horário com o qual se vai comparar.
+     * @return true se os horários forem iguais, false caso contrário.
+     */
+    bool operator==(const Schedule& s) const;
+
+    /**
+     * @brief Retorna uma representação em cadeia de texto do horário.
+     * @return Cadeia de texto com informações sobre as aulas no horário.
+     */
     std::string print();
 
-    //gets
+    // Getters
+
+    /**
+     * @brief Obtém o vetor de aulas no horário.
+     * @return Vetor de aulas no horário.
+     */
     std::vector<Lesson> get_Schedule();
 
-    //methods
+    // Métodos
+
+    /**
+     * @brief Adiciona uma aula ao horário.
+     * @param aula Aula a ser adicionada ao horário.
+     */
     void add_Lesson(Lesson aula);
+
+    /**
+     * @brief Remove uma aula do horário.
+     * @param aula Aula a ser removida do horário.
+     */
     void remove_Lesson(Lesson aula);
+
+    /**
+     * @brief Ordena as aulas no horário com base em algum critério específico.
+     */
     void sorted();
 };
 
-
-#endif //AED_PROJECT_SCHEDULE_H
+#endif // AED_PROJECT_SCHEDULE_H
--- Classes/Student.h
+++ Classes/Student.h
@@ -1,34 +1,107 @@
 #ifndef AED_PROJECT_STUDENT_H
 #define AED_PROJECT_STUDENT_H
+
 #include <string>
 #include <vector>
 #include "Schedule.h"
 
+/**
+ * @class Student
+ * @brief Classe que representa um estudante com informações sobre o seu código, nome e horário.
+ */
 class Student {
 public:
-    Student(std::string StudentCode_,std::string StudentName_,Schedule horario);
-    Student(std::string StudentCode_,std::string StudentName_);
+    /**
+     * @brief Construtor que inicializa um objeto Student com código, nome e horário.
+     * @param StudentCode_ Código do estudante.
+     * @param StudentName_ Nome do estudante.
+     * @param horario Horário do estudante (objeto Schedule).
+     */
+    Student(std::string StudentCode_, std::string StudentName_, Schedule horario);
+
+    /**
+     * @brief Construtor que inicializa um objeto Student com código e nome.
+     * @param StudentCode_ Código do estudante.
+     * @param StudentName_ Nome do estudante.
+     */
+    Student(std::string StudentCode_, std::string StudentName_);
+
+    /**
+     * @brief Construtor padrão que inicializa um objeto Student sem informações.
+     */
     Student();
+
+    /**
+     * @brief Destrutor da classe Student.
+     */
     ~Student();
 
-    //operators
-    bool operator == (const Student& s) const;
+    // Operadores
+
+    /**
+     * @brief Sobrecarga do operador de igualdade para comparar dois objetos Student.
+     * @param s Estudante com o qual se vai comparar.
+     * @return true se os estudantes forem iguais, false caso contrário.
+     */
+    bool operator==(const Student& s) const;
+
+    /**
+     * @brief Sobrecarga do operador de comparação para determinar a ordem dos estudantes.
+     * @param other Outro estudante com o qual se vai comparar.
+     * @return true se o estudante atual for menor do que o outro, false caso contrário.
+     */
     bool operator<(const Student& other) const;
+
+    /**
+     * @brief Devolve uma representação em cadeia de texto dos dados do estudante.
+     * @return Cadeia de texto com os dados do estudante.
+     */
     std::string print_Student();
-    //methods
+
+    // Métodos
+
+    /**
+     * @brief Adiciona uma lição (Lesson) ao horário do estudante.
+     * @param aula Lição que se vai adicionar ao horário do estudante.
+     */
     void add_Lesson(Lesson aula);
+
+    /**
+     * @brief Remove uma lição (Lesson) do horário do estudante.
+     * @param aula Lição que se vai remover do horário do estudante.
+     */
     void remove_Lesson(Lesson aula);
-    //gets
+
+    // Getters
+
+    /**
+     * @brief Obtém o código do estudante.
+     * @return Código do estudante.
+     */
     std::string get_StudentCode();
+
+    /**
+     * @brief Obtém o nome do estudante.
+     * @return Nome do estudante.
+     */
     std::string get_StudentName();
+
+    /**
+     * @brief Obtém o horário do estudante.
+     * @return Horário do estudante (objeto Schedule).
+     */
     Schedule get_Horario();
+
+    /**
+     * @brief Obtém uma lista das aulas do estudante.
+     * @return Vetor de cadeias de texto que representa as aulas do estudante.
+     */
     std::vector<std::string> get_classes();
 
 private:
-    std::string StudentCode;
-    std::string StudentName;
-    Schedule horario_student;
+    std::string StudentCode;  ///< Código do estudante.
+    std. O nome do estudante é o nome do estudante.
+    Schedule horario_student;  ///< Horário do estudante (objeto Schedule).
 };
 
-
-#endif //AED_PROJECT_STUDENT_H
+#endif // AED_PROJECT_STUDENT_H
